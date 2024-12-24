51c911e2aa3f1baec8b1942cc9a71a5df7bbc4e1
elbekjumayev <96642567+elbekjumayev@users.noreply.github.com>
2024-12-24 21:48:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from fastai.vision.all import *
 import plotly.express as px
 import platform 
 import pathlib
-temp=pathlib.PosixPath
-pathlib.PosixPath=pathlib.WindowsPath
 
 plt=platform.system()
 if plt=='Linux': pathlib.WindowsPath=pathlib.PosixPath
