4b0ecc82ba38e113426b6e2d3f066269d046b22a
elbekjumayev <elbek6imi@gmail.com>
2024-12-24 21:06:13 +0500

    project files have been uploaded

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+import platform 
+import pathlib
+temp=pathlib.PosixPath
+pathlib.PosixPath=pathlib.WindowsPath
+
+plt=platform.system()
+if plt=='Linux': pathlib.WindowsPath=pathlib.PosixPath
+
+st.title("Ayiq, qushlar va mevalarni aniqlovchi model")
+file=st.file_uploader("Rasmni yuklash", type=['png','jpg','jpeg','gif','webp'])
+
+if file:
+    st.image(file)
+    img=PILImage.create(file)
+    model=load_learner('BFBmodel.pkl')
+    pred, pred_id, probs=model.predict(img)
+    st.success(f'Bu rasm: {pred}')
+    st.info(f'Aniqlik darajasi: {probs[pred_id]*100:.2f}%')
+    fig=px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
